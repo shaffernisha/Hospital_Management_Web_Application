335b9948f18e1e138240e0b06e043c0a4e5d1af1
shaffernisha <shaffernisha96@gmail.com>
2026-03-23 11:08:13 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://hospital-management-web-application-lrt7fckig.vercel.app/',
     credentials: true,
   })
 );
